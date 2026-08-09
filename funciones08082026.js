5340d02690f2ea13fd6d9b5ab832d664b3772ef6
LovenistaXx <neoclassiclove@gmail.com>
2026-08-08 20:21:44 -0400

    Update funciones08082026.js

--- funciones08082026.js
+++ funciones08082026.js
@@ -1,21 +1,48 @@
-function togglePlay(btn) {
-    var playerCard = btn.closest('.VdM-SinglePlayer');
-    var audio = playerCard.querySelector('.VdM-audio-src');
-    var icon = btn.querySelector('i');
+if (!window.YT) {
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
 
-    if (audio.paused) {
-        // Pausa otros audios para que no suenen dos a la vez
-        document.querySelectorAll('.VdM-audio-src').forEach(a => {
-            if(a !== audio) {
-                a.pause();
-                a.closest('.VdM-SinglePlayer').querySelector('.VdM-play-btn i').className = "fa-solid fa-play";
-            }
+function onYouTubeIframeAPIReady() {
+    document.querySelectorAll('.VdM-MiniPlayer').forEach(function(container, index) {
+        var videoId = container.getAttribute('data-video-id');
+        var frameId = 'vdm-mini-' + index;
+        
+        var div = document.createElement('div');
+        div.id = frameId;
+        div.className = 'yt-hidden-frame';
+        container.appendChild(div);
+
+        var player = new YT.Player(frameId, {
+            height: '0',
+            width: '0',
+            videoId: videoId,
+            playerVars: { 'autoplay': 0, 'controls': 0 }
         });
 
-        audio.play();
-        icon.className = "fa-solid fa-pause";
-    } else {
-        audio.pause();
-        icon.className = "fa-solid fa-play";
-    }
+        container._player = player;
+
+        var playBtn = container.querySelector('.btn-play-mini');
+        playBtn.addEventListener('click', function() {
+            var state = player.getPlayerState();
+            var icon = playBtn.querySelector('i');
+
+            if (state == YT.PlayerState.PLAYING) {
+                player.pauseVideo();
+                icon.className = "fa-solid fa-play";
+            } else {
+                document.querySelectorAll('.VdM-MiniPlayer').forEach(function(c) {
+                    if (c._player && c !== container) {
+                        c._player.pauseVideo();
+                        var otherIcon = c.querySelector('.btn-play-mini i');
+                        if(otherIcon) otherIcon.className = "fa-solid fa-play";
+                    }
+                });
+                player.playVideo();
+                icon.className = "fa-solid fa-pause";
+            }
+        });
+    });
 }
