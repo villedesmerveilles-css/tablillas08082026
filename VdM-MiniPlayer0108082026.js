6174cd1f7d2786ef8df833c6bc554a75e300e30f
LovenistaXx <neoclassiclove@gmail.com>
2026-08-08 21:52:49 -0400

    Create VdM-MiniPlayer0108082026.js

---
+++ VdM-MiniPlayer0108082026.js
@@ -0,0 +1,83 @@
+if (!window.YT) {
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
+
+function onYouTubeIframeAPIReady() {
+    document.querySelectorAll('.VdM-MiniPlayer').forEach(function(container, index) {
+        var videoId = container.getAttribute('data-video-id');
+        var frameId = 'vdm-mini-' + index;
+        
+        var div = document.createElement('div');
+        div.id = frameId;
+        div.className = 'yt-hidden-frame';
+        container.appendChild(div);
+
+        // Crear barra de progreso en el HTML dinámicamente si no existe
+        if (!container.querySelector('.vdm-progress-bar')) {
+            var progressBar = document.createElement('div');
+            progressBar.className = 'vdm-progress-bar';
+            progressBar.innerHTML = '<div class="vdm-progress-fill"></div>';
+            container.appendChild(progressBar);
+        }
+
+        var fillBar = container.querySelector('.vdm-progress-fill');
+        var player, progressInterval;
+
+        var player = new YT.Player(frameId, {
+            height: '1',
+            width: '1',
+            videoId: videoId,
+            playerVars: { 
+                'autoplay': 0, 
+                'controls': 0,
+                'enablejsapi': 1,
+                'origin': window.location.origin
+            },
+            events: {
+                'onReady': function(event) {
+                    container._player = event.target;
+                },
+                'onStateChange': function(event) {
+                    if (event.data == YT.PlayerState.PLAYING) {
+                        progressInterval = setInterval(function() {
+                            var current = player.getCurrentTime();
+                            var total = player.getDuration();
+                            if (total > 0) {
+                                var percent = (current / total) * 100;
+                                fillBar.style.width = percent + '%';
+                            }
+                        }, 500);
+                    } else {
+                        clearInterval(progressInterval);
+                    }
+                }
+            }
+        });
+
+        var playBtn = container.querySelector('.btn-play-mini');
+        playBtn.addEventListener('click', function() {
+            if (!container._player) return;
+
+            var state = player.getPlayerState();
+            var icon = playBtn.querySelector('i');
+
+            if (state === YT.PlayerState.PLAYING || state === YT.PlayerState.BUFFERING) {
+                player.pauseVideo();
+                icon.className = "fa-solid fa-play";
+            } else {
+                document.querySelectorAll('.VdM-MiniPlayer').forEach(function(c) {
+                    if (c._player && c !== container) {
+                        c._player.pauseVideo();
+                        var otherIcon = c.querySelector('.btn-play-mini i');
+                        if(otherIcon) otherIcon.className = "fa-solid fa-play";
+                    }
+                });
+                player.playVideo();
+                icon.className = "fa-solid fa-pause";
+            }
+        });
+    });
+}
